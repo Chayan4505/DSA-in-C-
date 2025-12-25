9c0d7e05d61d69709934f4ae33975f3dd4b9b400
Chayan Mal <chayan2005mal@gmail.com>
2025-12-25 16:22:39 +0530

    Add files via upload
    Hashing

---
+++ Largest_SubArray_Sum-0.cpp
@@ -0,0 +1,34 @@
+ #include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<unordered_set>
+#include<string>
+using namespace std;
+
+int largestSubWith0Sum(vector<int> arr){
+    unordered_map<int, int> m;// <sum, idx >
+
+    int sum = 0;
+    int ans = 0;
+    for (int j = 0; j < arr.size(); j++)
+    {
+        sum += arr[j];
+
+        if (m.count(sum))
+        {
+            int currLen = j- m[sum];    // j - idx
+            ans = max(ans, currLen);
+        }
+        else{
+            m[sum] = j;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {15,-2,2,-8,1,7,10};
+    
+    cout<<"Ans = "<<largestSubWith0Sum(arr)<<endl;
+    return 0;
+}
